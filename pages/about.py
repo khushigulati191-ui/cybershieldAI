da7ff5688b6d527d8ac18cb678eb325e223b7c7b
khushigulati191-ui <khushigulati191@gmail.com>
2026-07-07 23:58:20 +0530

    added nav bar

---
+++ pages/about.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.write("This is the about page. You can find information about the CyberShield AI project here.")
---
+++ pages/compare.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.write("This is the compare page. You can compare the security and privacy analysis of two websites here.")
